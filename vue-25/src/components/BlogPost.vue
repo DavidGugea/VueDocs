b62ae3246cd0ca46649d1b1ee3fa3e84e6e44304
David Gugea <davidgugea@yahoo.com>
2022-11-07 19:01:30 +0100

    Vue Docs

---
+++ vue-25/src/components/BlogPost.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :style="blogPostStyle">
+        <p>Title: {{ title }}</p>
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+defineProps(["title"]);
+
+const blogPostStyle = {
+    border: "5px blue solid",
+    padding: "10px",
+    margin: "10px",
+};
+</script>
